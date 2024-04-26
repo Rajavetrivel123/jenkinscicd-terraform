0353dde67bd07b6bd1bba0dcf02d197e26e2b0fb
manikandan <rajavetrivelmani@gmail.com>
2024-04-26 12:27:47 +0530

    jenkins with tf files

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  type    = "string"
+  default = "ap-south-1"
+}
+variable "ami_id" {
+  type = "map"
+  default = {
+    ap-south-1    = "ami-007020fd9c84e18c7"
+    ap-southeast-1    = "ami-06c4be2792f419b7b"
+    ap-northeast-1 = "ami-0eba6c58b7918d3a1"
+  }
+}
+variable "instance_type" {
+  type    = "string"
+  default = "t2.micro"
+}
+variable "key_name" {
+  type    = "string"
+  default = "dpp"
+}
