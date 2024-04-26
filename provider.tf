3034be2bf5b5d5b71ae5ae5ade4d800fbbb6ad59
manikandan <rajavetrivelmani@gmail.com>
2024-04-26 19:27:16 +0530

    qa

--- provider.tf
+++ provider.tf
@@ -2,8 +2,9 @@ terraform {
   # Configure the S3 backend
   backend "s3" {
     bucket = "terraform.s3.22-04-24"
-    key    = "tfstate/statefile.tf"
+    key    = "tfstate"
     region = "ap-south-1"
+    dynamodb_table = "terraforms"
   }
   required_providers {
     aws = {
