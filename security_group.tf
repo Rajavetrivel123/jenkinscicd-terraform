cf34ef2fba1353439181c99d4ca4098e558c786e
manikandan <rajavetrivelmani@gmail.com>
2024-06-26 20:09:26 +0530

    remove sg port no

--- security_group.tf
+++ security_group.tf
@@ -4,8 +4,8 @@ resource "aws_security_group" "allow_ssh" {
   #vpc_id      = aws_vpc.vpc_demo.id
 
   ingress {
-    # SSH Port 22 allowed from any IP
-    from_port   = 22
+  
+    from_port   = 223
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
